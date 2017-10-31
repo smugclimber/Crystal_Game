45adda046d343ea9622b6a15b838cfdd1448112f
unknown <smugclimber@icloud.com>
2017-10-30 22:06:59 -0400

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,28 @@
 $(document).ready(function()
 {
     //// Variable Section  ====================================================
-    var crystal1 = {value: 1};
-    var crystal2 = {value: 2};
-    var crystal3 = {value: 3};
-    var crystal4 = {value: 4};
-    var wins = 0;
-    var losses = 0;
+    var crystal1 = 1;
+    var crystal2 = 2;
+    var crystal3 = 3;
+    var crystal4 = 4;
+    var winCount = 0;
+    var lossCount = 0;
     var score = 0;
     var target = 0;
-
     //// End of Var Section  ====================================================
 
     //// Function Declarations  ====================================================
     function newCrystValues()
     {
       var newValue = Math.floor((Math.random() * 12) + 1);
-      return newValue;
+      crystal1 = newValue;
+      newValue = Math.floor((Math.random() * 12) + 1);
+      crystal2 = newValue;
+      newValue = Math.floor((Math.random() * 12) + 1);
+      crystal3 = newValue;
+      newValue = Math.floor((Math.random() * 12) + 1);
+      crystal4 = newValue;
+      console.log(crystal1, crystal2, crystal3, crystal4);
     };
 
     function genTarget()
@@ -26,22 +32,55 @@ $(document).ready(function()
       $("#targetText").text(target);
     };
 
-    function updateGame()
+    function resetGame()
     {
-      ++winCount;
-      // win to winText
-      ++loseCount
+      console.log("Wins count: " + winCount);
+      $("#winsText").text(winCount);
+      $("#lossesText").text(lossCount);
+      console.log("Losses count: " + lossCount);
       //write to loseText
       score = 0;
       genTarget();
       newCrystValues();
     };
 
-    $("#button1").on("click", function() {
-      score +=crystal1["value"];
-      //broke:scope not reaching global score
-      console.log(score);
+    function proceed()
+    {
+      if(score === target)
+      {
+        $("#scoreText").text("You Won!!");
+        alert("You're a Crystal God!");
+        ++winCount;
+        resetGame();
+      }else if(score > target)
+      {
+        $("#scoreText").text("You Lose!!");
+        alert("You've failed!! The cave has collapsed on you...try again?'");
+        ++lossCount;
+        resetGame();
+      }
+      console.log("New Score :"+score);
       $("#scoreText").text(score);
+    };
+
+    $(".button1").on("click", function() {
+      score +=crystal1;
+      proceed();
+    });
+
+    $(".button2").on("click", function() {
+      score +=crystal2;
+      proceed();
+    });
+
+    $(".button3").on("click", function() {
+      score +=crystal3;
+      proceed();
+    });
+
+    $(".button4").on("click", function() {
+      score +=crystal4;
+      proceed();
     });
     //// End of Funct Declarations  ====================================================
 
@@ -49,14 +88,7 @@ $(document).ready(function()
     genTarget();
     console.log("Score: "+score)
     console.log("target: "+target)
-    if(score === target)
-    {
-      $("#scoreText").text("You Won!!");
-      alert("You're a Crystal God!");
-      //updateGame();
-    };
-
-
-
+    newCrystValues();
+    console.log(crystal1, crystal2, crystal3, crystal4);
     //// End of Function Calls  ====================================================
 });
